098acd6df673a1ac5adda9257ecb78bd21623d7b
livechimney <adrianmuriuki1366@gmail.com>
2023-11-02 08:38:06 -0700

    simple shell 4

---
+++ fwe.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void)
+{
+	pid_t child_pid;
+	int status;
+	int i;
+	char *args[] = {"ls", "-1", "/tmp", NULL};
+
+	for (i = 1; i <= 5; i++)
+	{
+		child_pid = fork();
+		if (child_pid == -1)
+		{
+			perror("Error:");
+			return (1);
+		}
+	}
+
+	if (child_pid == 0)
+	{
+		execve("/usr/bin/ls", args, NULL);
+		perror("Error:");
+		exit(1);
+
+	}
+	else
+	{
+		/*parent*/
+		wait(&status);
+	}
+}
